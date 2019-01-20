e4721524cc7919260aaff98d18f1e4c3461722d3
Robert Valik <valik@visitors.sk>
2019-01-20 08:04:30 +0100

    Some if/then/else reorg

--- support/chat.go
+++ support/chat.go
@@ -17,38 +17,35 @@ func Chat(s *discordgo.Session, m *discordgo.MessageCreate) {
 			ErrorLog(fmt.Errorf("%s: An error occurred when attempting to tail factorio.log\nDetails: %s", time.Now(), err))
 		}
 		for line := range t.Lines {
-			if strings.Contains(line.Text, "[CHAT]") || strings.Contains(line.Text, "[JOIN]") || strings.Contains(line.Text, "[LEAVE]") || strings.Contains(line.Text, "FactoCord.ext: ") {
-				if !strings.Contains(line.Text, "<server>") {
+			if strings.Contains(line.Text, "[CHAT]") {
+				if !strings.Contains(line.Text, "<server>: [Discord]") {
+					TmpList := strings.Split(line.Text, " ")
+					TmpList[3] = strings.Replace(TmpList[3], ":", "", -1)
+					if strings.Contains(strings.Join(TmpList, " "), "@") {
+						index := LocateMentionPosition(TmpList)
 
-					if strings.Contains(line.Text, "[JOIN]") ||
-						strings.Contains(line.Text, "[LEAVE]") {
-						TmpList := strings.Split(line.Text, " ")
-						// Don't hard code the channelID! }:<
-						s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("%s", strings.Join(TmpList[3:], " ")))
-					} else if strings.Contains(line.Text, "FactoCord.ext:") {
-						TmpList := strings.Split(line.Text, " ")
-						s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("%s", strings.Join(TmpList[1:], " ")))
-					} else {
+						for _, position := range index {
+							User := SearchForUser(TmpList[position])
 
-						TmpList := strings.Split(line.Text, " ")
-						TmpList[3] = strings.Replace(TmpList[3], ":", "", -1)
-						if strings.Contains(strings.Join(TmpList, " "), "@") {
-							index := LocateMentionPosition(TmpList)
-
-							for _, position := range index {
-								User := SearchForUser(TmpList[position])
-
-								if User == nil {
-									continue
-								}
-								TmpList[position] = User.Mention()
+							if User == nil {
+								continue
 							}
-
+							TmpList[position] = User.Mention()
 						}
 
-						s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("<%s>: %s", TmpList[3], strings.Join(TmpList[4:], " ")))
 					}
+					s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("<%s>: %s", TmpList[3], strings.Join(TmpList[4:], " ")))
 				}
+
+			} else if strings.Contains(line.Text, "[JOIN]") ||
+				strings.Contains(line.Text, "[LEAVE]") {
+				TmpList := strings.Split(line.Text, " ")
+				s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("%s", strings.Join(TmpList[3:], " ")))
+
+			} else if strings.Contains(line.Text, "FactoCord.ext: ") {
+				TmpList := strings.Split(line.Text, " ")
+				s.ChannelMessageSend(Config.FactorioChannelID, fmt.Sprintf("%s", strings.Join(TmpList[1:], " ")))
+
 			} else {
 				if strings.Contains(Config.LogConsole, "TRUE") {
 					s.ChannelMessageSend(Config.FactorioConsoleChannelID, fmt.Sprintf("%s", line.Text))
